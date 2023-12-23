08a6d5a1d9ed36f1a0c1f4d3739798fc529fba38
sebastienserre <sebastien@thivinfo.com>
2023-12-23 10:43:48 +0100

    auto create debug.log if not exists

--- clean-lando.php
+++ clean-lando.php
@@ -1,40 +1,44 @@
 <?php
 require __DIR__ . '/conf.php';
-$listdir = scandir( DIR );
 
-error_log(PHP_EOL . date( 'd-m-Y-H-i-s'). PHP_EOL, 3,  DIR . '/debug.log');
-remove_old_images( 30, DIR );
-if (! empty($listdir) ){
+create_debug();
+clean_lando();
 
-	foreach ( $listdir as $key => $folder_name ){
-		$path = DIR . '/' . $folder_name;
-		$do_not_destroy = array(
+function clean_lando() {
+	$listdir = scandir( DIR );
 
-		);
-		if ( in_array( $folder_name, KEEP_ALIVE ) ){
-			continue;
-		}
-		$fileinfo = stat( $path );
-		$last_access = $fileinfo['atime'];
-		$now = time();
-		$day = ($now - $last_access)/86400;
+	error_log( PHP_EOL . date( 'd-m-Y-H-i-s' ) . PHP_EOL, 3, DIR . '/debug.log' );
+	remove_old_images( 30, DIR );
+	if ( ! empty( $listdir ) ) {
 
-		if ( $day >= 30 ){
-			chdir( $path );
-			if ( file_exists( $path . '/.lando.yml' ) ) {
-
-				exec( 'lando destroy -y' );
-				rrmdir( $path );
-				echo "destroyed";
-				error_log($folder_name . ' has been destroyed'. PHP_EOL, 3,  DIR . '/debug.log');
-
-			} else {
-				echo $folder_name . ' is not a lando' . PHP_EOL;
-				error_log($folder_name . ' is not a lando'. PHP_EOL, 3,  DIR . '/debug.log');
+		foreach ( $listdir as $key => $folder_name ) {
+			$path           = DIR . '/' . $folder_name;
+			$do_not_destroy = array();
+			if ( in_array( $folder_name, KEEP_ALIVE ) ) {
+				continue;
 			}
-		} else {
-			error_log( $folder_name . ' is too young to die' . PHP_EOL, 3,  DIR . '/debug.log');
-			echo $folder_name . ' is too young to die' . PHP_EOL;
+			$fileinfo    = stat( $path );
+			$last_access = $fileinfo['atime'];
+			$now         = time();
+			$day         = ( $now - $last_access ) / 86400;
+
+			if ( $day >= 30 ) {
+				chdir( $path );
+				if ( file_exists( $path . '/.lando.yml' ) ) {
+
+					exec( 'lando destroy -y' );
+					rrmdir( $path );
+					echo "destroyed";
+					error_log( $folder_name . ' has been destroyed' . PHP_EOL, 3, DIR . '/debug.log' );
+
+				} else {
+					echo $folder_name . ' is not a lando' . PHP_EOL;
+					error_log( $folder_name . ' is not a lando' . PHP_EOL, 3, DIR . '/debug.log' );
+				}
+			} else {
+				error_log( $folder_name . ' is too young to die' . PHP_EOL, 3, DIR . '/debug.log' );
+				echo $folder_name . ' is too young to die' . PHP_EOL;
+			}
 		}
 	}
 }
@@ -59,4 +63,16 @@ function remove_old_images( $days = 30 ){
 	$timestamp = time() - $days * 86400;
 	$reclaimed = exec( 'docker image prune -af --filter until=' . $timestamp);
 	error_log( $reclaimed . ' by docker image prune'. PHP_EOL, 3,  DIR . '/debug.log');
+	echo $reclaimed . ' by docker image prune'. PHP_EOL;
+}
+
+function create_debug(){
+	if ( file_exists( DIR . 'debug.log' ) ){
+		error_log( 'debug.log already exists'. PHP_EOL, 3,  DIR . '/debug.log');
+		echo 'debug.log already exists'. PHP_EOL;
+		return;
+	}
+	echo 'debug.log dont exists'. PHP_EOL;
+
+	file_put_contents( DIR . '/debug.log', 'start', FILE_APPEND );
 }
